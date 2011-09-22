20b5a3eb06d3d9a8a9b350a893c5d57f7169b6a9
Doug Turner <dougt@dougt.org>
2011-09-22 01:15:10 -0700

    1) Fixes a rotation crash.  Basically any layout you have, you must remove all child views before destorying it.

--- embedding/android/GeckoApp.java
+++ embedding/android/GeckoApp.java
@@ -400,7 +400,8 @@ abstract public class GeckoApp
 
         if (surfaceView == null)
             surfaceView = new GeckoSurfaceView(this);
-        else
+
+        if (mainLayout != null)
             mainLayout.removeAllViews();
 
         mainLayout = new LinearLayout(this);
@@ -447,6 +448,9 @@ abstract public class GeckoApp
         addressBar.addView(reload);
 
         // setup gecko layout
+        if (geckoLayout != null)
+            geckoLayout.removeAllViews();
+
         geckoLayout = new AbsoluteLayout(this);
         mainLayout.addView(geckoLayout);
         
