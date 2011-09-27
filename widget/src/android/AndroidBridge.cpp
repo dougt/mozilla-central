1b24feb24f77c316947097102413cf92ceda8ca4
Doug Turner <dougt@dougt.org>
2011-09-27 16:05:59 -0700

    1) It is nice when you don't leak.  Adding a jni frame to prevent us from leaking strings.

--- widget/src/android/AndroidBridge.cpp
+++ widget/src/android/AndroidBridge.cpp
@@ -980,6 +980,7 @@ AndroidBridge::HandleGeckoMessage(const nsAString& aMessage)
         return;
     }
 
+    AutoLocalJNIFrame jniFrame(1);
     jstring jMessage = mJNIEnv->NewString(nsPromiseFlatString(aMessage).get(), aMessage.Length());
     env->CallStaticVoidMethod(mGeckoAppShellClass, jHandleGeckoMessage, jMessage);
 
