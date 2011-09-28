298cfea8839b08f1dd5f80799b28fc980d610db4
Doug Turner <dougt@dougt.org>
2011-09-27 20:42:54 -0700

    1) moving the script from browser.xul -> browser.js 2) forwarding onStateChange 3) a bit of tweaking to the throbber

--- embedding/android/GeckoAppShell.java
+++ embedding/android/GeckoAppShell.java
@@ -1719,6 +1719,15 @@ public class GeckoAppShell
                     });
                 Log.i("GeckoShell", "URI - " + value);
             }
+            else if (type.equals("onStateChange")) {
+                String state = geckoObject.getString("state");
+                String stateIs = geckoObject.getString("stateIs");
+
+                if (state == "start") {
+                    GeckoApp.mProgressBar.setVisibility(View.VISIBLE);
+                    GeckoApp.mProgressBar.setIndeterminate(true);
+                }
+            }
             else if (type.equals("onProgressChange")) {
                 final int current = geckoObject.getInt("current");
                 final int total = geckoObject.getInt("total");
@@ -1726,17 +1735,14 @@ public class GeckoAppShell
                 getMainHandler().post(new Runnable() { 
                         public void run() {
                             if (total == -1) {
-                                GeckoApp.mProgressBar.setVisibility(View.VISIBLE);
                                 GeckoApp.mProgressBar.setIndeterminate(true);
                             } else if (current < total) {
-                                GeckoApp.mProgressBar.setVisibility(View.VISIBLE);
                                 GeckoApp.mProgressBar.setIndeterminate(false);
                                 GeckoApp.mProgressBar.setMax(total);
                                 GeckoApp.mProgressBar.setProgress(current);
                             }
                             else {
                                 GeckoApp.mProgressBar.setIndeterminate(false);
-                                GeckoApp.mProgressBar.setVisibility(View.GONE);
                             }
                         }
                     });
