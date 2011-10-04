429f8e853374ebdf0660aedb80f605c085516110
Brian Nicholson <bnicholson@mozilla.com>
2011-10-03 21:26:46 -0700

    added awesomebar autocompletion and basic history

--- mobile/chrome/content/browser.js
+++ mobile/chrome/content/browser.js
@@ -128,6 +128,17 @@ FennecProgressListener.prototype = {
                 }};
             
             SendMessageToJava(message);
+            
+            if (uri == Services.prefs.getCharPref("browser.last.uri")) {
+                let showMessage = {
+                    "gecko": {
+                        "type"       : "hideLoadingScreen",
+                        "windowID"   : windowID,
+                    }};
+                
+                SendMessageToJava(showMessage);
+            }
+
         } catch (e) {dump("onLocationChange throws " + e); }
     },
 
