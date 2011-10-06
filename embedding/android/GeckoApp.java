da754a5731ce581bcd4e5c22ab0e9ffd757dea05
Gian-Carlo Pascutto <gpascutto@mozilla.com>
2011-10-06 14:31:09 +0200

    Add reload button fuctionality. Just reload last URL in history.

--- embedding/android/GeckoApp.java
+++ embedding/android/GeckoApp.java
@@ -465,6 +465,11 @@ abstract public class GeckoApp
 
         ImageButton reload = new ImageButton(this);
         reload.setImageResource(R.drawable.reload);
+        reload.setOnClickListener(new ImageButton.OnClickListener() {
+                public void onClick(View v) {
+                    doReload();
+                }
+            });
         addressBar.addView(reload);
 
         // setup gecko layout
@@ -838,6 +843,13 @@ abstract public class GeckoApp
         searchIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION | Intent.FLAG_ACTIVITY_NO_HISTORY);
 
         startActivityForResult(searchIntent, AWESOMEBAR_REQUEST);
+        return true;
+    }
+
+    public boolean doReload() {
+        Log.i("GeckoApp", "Reload requested");
+        String currUrl = sessionHistory.peek();
+        GeckoAppShell.sendEventToGecko(new GeckoEvent(currUrl));
         return true;
     }
 
